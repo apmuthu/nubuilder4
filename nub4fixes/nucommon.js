edbf26260b8caa5643a5499453ef8ba7003e5a83
Ap.Muthu <apmuthu@usa.net>
2020-12-06 20:21:23 +0530

    Fixed search keys F3 and CTRL+f by resetting window.nuNEW=0 https://github.com/smalos/nuBuilder4-Bug-Tracker/commit/834aa0c3c67b857fc9a62b29874150356630a9bd#commitcomment-44850769

--- nub4fixes/nucommon.js
+++ nub4fixes/nucommon.js
@@ -530,11 +530,16 @@ function nuRunIt(t, email, type){
 function nuBindCtrlEvents(){
 
 	var nuCtrlKeydownListener = function(e){	
-	
-		if(e.keyCode == 17) { //Ctrl
-			window.nuNEW = 1;
+
+		if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) { // exclude search keys F3 and Ctrl + f
+			window.nuNEW = 0;
+		} else {
+			if (e.keyCode == 17) { //Ctrl                         
+				window.nuNEW = 1;
+			}
+
 		}
-		
+
 	}
 	
 	$(document).keydown(function(e) {
